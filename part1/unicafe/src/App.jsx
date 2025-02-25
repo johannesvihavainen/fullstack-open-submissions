c632472fd4d64286f34772c95178578231503ab7
Johannes Vihavainen <johannesvihavainen@icloud.com>
2025-02-25 21:59:58 +0200

    Add unicafe step 2 - 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -20,6 +20,20 @@ const App = () => {
     setBad(previousValue => previousValue + 1)
   }
 
+  const CalculateAverage = ({ good, neutral, bad }) => {
+    let average = (good - bad) / (good + neutral + bad)
+    return (
+      <p>average {average !== 0 && !isNaN(average) ? average : null}</p>
+    )
+  }
+
+  const CalculatePositive = () => {
+    let positive = (good / (good + neutral + bad)) * 100
+    return (
+      <p>positive {positive !== 0 && !isNaN(positive) ? positive + ' %': null}</p>
+    )
+  }
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -32,6 +46,9 @@ const App = () => {
       <p>good {good !== 0 ? good : null}</p>
       <p>neutral {neutral !== 0 ? neutral : null}</p>
       <p>bad {bad !== 0 ? bad : null}</p>
+      <p>all {good + neutral + bad !== 0 ? good + neutral + bad : null}</p>
+      <CalculateAverage good={good} neutral={neutral} bad={bad} />
+      <CalculatePositive />
     </div>
   )
 }
