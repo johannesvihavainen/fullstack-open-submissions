80c3980df6f76ca922c2d886c97f455a47f048c7
Johannes Vihavainen <johannesvihavainen@icloud.com>
2025-03-09 12:13:59 +0200

    add helper functions and unit tests - step 4 - 4.6*

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -12,8 +12,27 @@ const favoriteBlog = (blogs) => {
   return mostLikes
 }
 
+const mostBlogs = (blogs) => {
+
+  const blogCount = blogs.reduce((acc, blog) => {
+    acc[blog.author] = (acc[blog.author] || 0) + 1
+    return acc
+  }, {})
+
+  const authorWithMostBlogs = Object.keys(blogCount).reduce((max, author) => {
+    return blogCount[author] > blogCount[max] ? author : max
+  }, Object.keys(blogCount)[0])
+
+
+  return {
+    author: authorWithMostBlogs,
+    blogs: blogCount[authorWithMostBlogs]
+  }
+}
+
 module.exports = {
-  dummy, 
+  dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 }
