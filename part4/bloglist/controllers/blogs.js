43dbd8222ac09e66fe6b0db80e7f03b330aece5e
Johannes Vihavainen <johannesvihavainen@icloud.com>
2025-03-11 21:21:34 +0200

    add blog list expansions, step 1 - 4.13

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -3,22 +3,32 @@ const Blog = require('../models/blog')
 const router = express.Router()
 
 router.get('/', async (request, response) => {
-const blogs = await Blog.find({})
-response.json(blogs)
+    const blogs = await Blog.find({})
+    response.json(blogs)
 })
 
 router.post('/', async (request, response) => {
 
-    const {title, url, author, likes} = request.body
-    
-    if(!title || !url) {
-        return response.status(400).json({error: 'the title and url are required in a blog post'})
+    const { title, url, author, likes } = request.body
+
+    if (!title || !url) {
+        return response.status(400).json({ error: 'the title and url are required in a blog post' })
     }
-    
+
     const blog = new Blog(request.body)
 
     const saveBlog = await blog.save()
     response.status(201).json(saveBlog)
+})
+
+router.delete('/:id', async (request, response) => {
+    try {
+        const { id } = request.params
+        await Blog.findByIdAndDelete(id)
+        response.status(204).end()
+    } catch (error) {
+        response.status(400).json({ error: 'invalid id' })
+    }
 })
 
 module.exports = router
--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -1,6 +1,6 @@
 process.env.NODE_ENV = 'test'
 console.log('NODE_ENV:', process.env.NODE_ENV)
-const { test, after } = require('node:test')
+const { test, after, beforeEach, describe } = require('node:test')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
@@ -9,110 +9,150 @@ const assert = require('node:assert/strict')
 
 const api = supertest(app)
 
-test('blogs are returned as json and contain the right amount of blog posts', async () => {
+// test('blogs are returned as json and contain the right amount of blog posts', async () => {
 
-  const blogsAtStart = await Blog.find({})
+//   const blogsAtStart = await Blog.find({})
 
-  const response = await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+//   const response = await api
+//     .get('/api/blogs')
+//     .expect(200)
+//     .expect('Content-Type', /application\/json/)
 
-  console.log(response.body)
+//   console.log(response.body)
 
-  assert.strictEqual(response.body.length, blogsAtStart.length)
-})
+//   assert.strictEqual(response.body.length, blogsAtStart.length)
+// })
 
-test('blog posts contain id field instead of being _id', async () => {
-  const response = await api.get('/api/blogs')
+// test('blog posts contain id field instead of being _id', async () => {
+//   const response = await api.get('/api/blogs')
 
-  console.log('logging response body');
+//   console.log('logging response body');
 
-  console.log(response.body[0]);
-
-
-
-  response.body.forEach(blog => {
-    assert.ok(blog.id, 'expected blog to contain an id property')
-    assert.strictEqual(typeof blog.id, 'string', 'id should be a string')
-    assert.strictEqual(blog._id, undefined, '_id should not exist')
-  });
+//   console.log(response.body[0]);
 
 
-  console.log(response.body)
-})
+
+//   response.body.forEach(blog => {
+//     assert.ok(blog.id, 'expected blog to contain an id property')
+//     assert.strictEqual(typeof blog.id, 'string', 'id should be a string')
+//     assert.strictEqual(blog._id, undefined, '_id should not exist')
+//   });
 
 
-test('a valid blog post can be added ', async () => {
+//   console.log(response.body)
+// })
 
-  const blogsAtStart = await Blog.find({})
 
-  const newBlogPost = {
-    title: "Do the hard work especially when you don't feel like it.",
-    author: "someone from somewhere once upon a time",
-    url: "nolinktoanywhere.com",
-    likes: 999999999
+// test('a valid blog post can be added ', async () => {
+
+//   const blogsAtStart = await Blog.find({})
+
+//   const newBlogPost = {
+//     title: "Do the hard work especially when you don't feel like it.",
+//     author: "someone from somewhere once upon a time",
+//     url: "nolinktoanywhere.com",
+//     likes: 999999999
+//   }
+
+//   await api
+//     .post('/api/blogs')
+//     .send(newBlogPost)
+//     .expect(201)
+//     .expect('Content-Type', /application\/json/)
+
+
+//   const blogsAtEnd = await Blog.find({})
+
+//   assert.strictEqual(blogsAtEnd.length, blogsAtStart.length + 1)
+
+//   const blogs = blogsAtEnd.map(blog => blog.title)
+//   assert.ok(blogs.includes(newBlogPost.title), 'the new blog was not saved in the right way')
+// })
+
+
+// test('if the likes is missing, then likes will default to 0', async () => {
+//   const newBlogPost = {
+//     title: "a blog post without likes",
+//     author: "some random guy",
+//     url: "nolinktoanywhere.com",
+//   }
+
+
+//   const response = await api
+//     .post('/api/blogs')
+//     .send(newBlogPost)
+//     .expect(201)
+
+//   const allBlogs = response.body
+
+//   assert.strictEqual(allBlogs.likes, 0, 'likes should be defaulting to 0 if they are missing')
+// })
+
+// test('blog post without a title will not be added', async () => {
+//   const newBlogPost = {
+//     author: "someone from somewhere once upon a time",
+//     url: "nolinktoanywhere.com",
+//     likes: 10
+//   }
+
+//   await api
+//     .post('/api/blogs')
+//     .send(newBlogPost)
+//     .expect(400)
+// })
+
+// test('blog post without a url will not be added', async () => {
+//   const newBlogPost = {
+//     title: 'author from testingland',
+//     author: "someone from somewhere once upon a time",
+//     likes: 10
+//   }
+
+//   await api
+//     .post('/api/blogs')
+//     .send(newBlogPost)
+//     .expect(400)
+// })
+
+const initialBlogsToUpload = [
+  {
+    title: 'a test blog',
+    author: 'an unknown person',
+    url: 'somewhereintheuniverse.com',
+    likes: 10
+  },
+  {
+    title: 'a second test blog',
+    author: 'an unknown human being',
+    url: 'somewhereintheuniverse.com',
+    likes: 20
   }
+]
+
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await Blog.insertMany(initialBlogsToUpload)
+})
+
+
+test('deletion of a blog post', async () => {
+  const blogsAtStart = await Blog.find({})
+  console.log(blogsAtStart);
+  
+  const blogToDelete = blogsAtStart[0]
 
   await api
-    .post('/api/blogs')
-    .send(newBlogPost)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
+    .delete(`/api/blogs/${blogToDelete.id}`)
+    .expect(204)
 
   const blogsAtEnd = await Blog.find({})
 
-  assert.strictEqual(blogsAtEnd.length, blogsAtStart.length + 1)
+  assert.strictEqual(blogsAtEnd.length, initialBlogsToUpload.length - 1)
 
-  const blogs = blogsAtEnd.map(blog => blog.title)
-  assert.ok(blogs.includes(newBlogPost.title), 'the new blog was not saved in the right way')
+  const ids = blogsAtEnd.map(blog => blog.id)
+  assert(!ids.includes(blogToDelete.id))
 })
 
-
-test('if the likes is missing, then likes will default to 0', async () => {
-  const newBlogPost = {
-    title: "a blog post without likes",
-    author: "some random guy",
-    url: "nolinktoanywhere.com",
-  }
-
-
-  const response = await api
-    .post('/api/blogs')
-    .send(newBlogPost)
-    .expect(201)
-
-  const allBlogs = response.body
-
-  assert.strictEqual(allBlogs.likes, 0, 'likes should be defaulting to 0 if they are missing')
-})
-
-test('blog post without a title will not be added', async () => {
-  const newBlogPost = {
-    author: "someone from somewhere once upon a time",
-    url: "nolinktoanywhere.com",
-    likes: 10
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlogPost)
-    .expect(400)
-})
-
-test('blog post without a url will not be added', async () => {
-  const newBlogPost = {
-    title: 'author from testingland',
-    author: "someone from somewhere once upon a time",
-    likes: 10
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlogPost)
-    .expect(400)
-})
 
 after(async () => {
   await mongoose.connection.close()
